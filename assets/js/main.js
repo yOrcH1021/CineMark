60dc17edc5c54406a3a5e8f7265db628c2a4f108
Jorge Olaya <edonkey122@hotmail.com>
2021-11-02 16:51:28 -0500

    many changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,6 @@
 
 const url = "https://yorch1021.github.io/CineMark/assets/js/datos.json"
 
-$("main > div").prepend('<div class="d-flex justify-content-center"><button class="btn mt-5">Ver Cartelera</button></div>');
-//Escuchamos el evento click del botón agregado
-$(".btn").click(() => { 
-    $.getJSON(url, function (answer, state) {
-        if(state === "success"){
-        let misDatos = answer;
-        for (const dato of misDatos) {
-            $("main > ul").prepend(`<li>
-                                <a href="${dato.url}" class="d-flex flex-column">
-                                    <img src="${dato.img}">
-                                    <p class="m-0">${dato.date}</p>
-                                    <h4 class="m-0">${dato.name}</h4>
-                                </a>
-                            </li>`)
-            }  
-        }
-    });
-    $("main > div").addClass("d-none");
-});
-
 $.getJSON(url, function (answer, state) {
     if(state === "success"){
     let misDatos = answer;
@@ -34,4 +14,9 @@ $.getJSON(url, function (answer, state) {
                         </li>`)
         }  
     }
+});
+
+$('.moreMovies').click(function() {
+    $('#movieList').addClass('allMovies');
+    this.remove();
 });
